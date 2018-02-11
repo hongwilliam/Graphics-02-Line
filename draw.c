ad380013f19dd86badb85edd0ba341f166bcb27c
hongwilliam <willhong20@gmail.com>
2018-02-11 02:24:58 -0500

    finished

--- draw.c
+++ draw.c
@@ -5,6 +5,121 @@
 #include "display.h"
 #include "draw.h"
 
+//assume line with defined slope
+double slope(int x, int y, int x1, int y1){
+  return ((double) y1 - y) / ((double) x1 - x); }
+
+//assume that x0 < x1 (subsequent pixel is to right)
+//return 1, 2, 7, 8 for octants
+int octant(int x, int y, int x1, int y1){
+  //note that we only need octs 1, 2, 7, 8
+  //5 is 1, 6 is 2, 4 is 8, 3 is 7
+
+  //undefined slope - vertical line
+  if (x == x1){
+    //octants 2 are closer to y axis
+    return 2; }
+
+  double line_slope = slope(x, y, x1, y1);
+
+  //octant 1
+  if (line_slope >= 0 && line_slope < 1){
+    return 1; }
+
+  //octant 2
+  else if (line_slope >= 1){
+    return 2; }
+
+  //octant 7
+  else if (line_slope < -1){
+    return 7;
+  }
+
+  //octant 8
+  else if (line_slope >= -1){
+    return 8; }
+
+  return 0; //error, debug code
+}
+
 //Insert your line algorithm here
-void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
+//assume that we work from left to right (x0 < x1)
+//we only need octants I, II, VII, VIII
+void draw_line(int x, int y, int x1, int y1, screen s, color c) {
+
+  if (x1 < x){
+    int temp = x;
+    x = x1;
+    x1 = temp;
+    temp = y;
+    y = y1;
+    y1 = temp; }
+
+  int line_octant = octant(x, y, x1, y1);
+  //use pseudo code from class
+  int d; ///2A + B, varies based on octant
+  int A = y1 - y, B = -(x1 - x); //A = delta y, B = negative delta x, constant for all octants
+
+  //straight from class
+  if (line_octant == 1){
+    d = (2 * A) + B;
+
+    while (x <= x1){
+      plot(s, c, x, y);
+
+      if (d > 0){
+        y++;
+        d += (2 * B); }
+
+      x++;
+      d += (2 * A);
+    }
+  }
+
+  //also again, straight from class
+  else if (line_octant == 2){
+    d = A + (2 * B);
+
+    while (y <= y1){
+      plot(s, c, x, y);
+
+      if (d < 0){
+        x++;
+        d += (2 * A); }
+
+      y++;
+      d += (2 * B);
+    }
+  }
+
+  else if (line_octant == 7) {
+    d = A - (2 * B);
+
+    while (y >= y1) {
+      plot(s, c, x, y);
+
+      if (d > 0){
+	       x++;
+	       d += (2 * A); }
+
+      y--;
+      d -= (2 * B);
+    }
+  }
+
+  else if (line_octant ==8 ) {
+    d = (2 * A) - B;
+
+    while (x <= x1){
+      plot(s, c, x, y);
+
+      if (d < 0){
+	       y--;
+	       d -= (2 * B); }
+
+      x++;
+      d += (2 * A);
+    }
+  }
+
 }
--- main.c
+++ main.c
@@ -9,14 +9,58 @@
 int main() {
 
   screen s;
-  color c;
+  color c1, c2, c3, c4, c5, c6, c7, c8;
 
-  c.red = 0;
-  c.green = MAX_COLOR;
-  c.blue = 0;
+  c1.red = 238;
+  c1.green = 130;
+  c1.blue = 238;
+
+  c2.red = 255;
+  c2.green = 0;
+  c2.blue = 255;
+
+  c3.red = 186;
+  c3.green = 85;
+  c3.blue = 211;
+
+  c4.red = 147;
+  c4.green = 112;
+  c4.blue = 219;
+
+  c5.red = 138;
+  c5.green = 43;
+  c5.blue = 226;
+
+  c6.red = 148;
+  c6.green = 0;
+  c6.blue = 211;
+
+  c7.red = 139;
+  c7.green = 0;
+  c7.blue = 139;
+
+  c8.red = 75;
+  c8.green = 0;
+  c8.blue = 130;
 
   clear_screen(s);
 
+  //now let the art commence - split coordiante plane into sixteenths
+  //setup x axis, y axis, y = x, y = -x
+  draw_line(0, 250, 500, 250, s, c1); //x axis
+  draw_line(250, 0, 250, 500, s, c1); //y axis
+  draw_line(0, 0, 500, 500, s, c2); //y = x
+  draw_line(0, 500, 500, 0, s, c1); //y = -x
+
+  draw_line(250, 250, 500, 375, s, c3); //octant I, slope = 0.5
+  draw_line(250, 250, 333, 500, s, c4); //octant II, slope = 1.51
+  draw_line(250, 250, 500, 125, s, c5); //octant VIII, slope = -0.5
+  draw_line(250, 250, 333, 0, s, c6); //octant VII, slope = -1.51
+
+  draw_line(250, 250, 0, 125, s, c3); //octant V
+  draw_line(250, 250, 167, 0, s, c4); //octant VI
+  draw_line(250, 250, 0, 375, s, c5); //octant IV
+  draw_line(250, 250, 167, 500, s, c6); //octant III
 
   display(s);
   save_extension(s, "lines.png");
